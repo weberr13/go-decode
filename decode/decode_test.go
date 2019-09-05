3d3de3e224b625040fcc0bd36a07854faf274abf
bonny <b.rais@f5.com>
2019-09-05 00:07:38 -0700

    changed to use FP as a paradigm for oneOf resolution and handling added tests to improve coverage

--- decode/decode_test.go
+++ decode/decode_test.go
@@ -7,9 +7,11 @@ package decode_test
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"testing"
+
 	. "github.com/smartystreets/goconvey/convey"
 	"github.com/weberr13/go-decode/decode"
-	"testing"
 )
 
 type SubRecord struct {
@@ -64,202 +66,7 @@ func NewRecord() interface{} {
 }
 
 type Envelope struct {
-	Pets []*BasePet
-}
-
-type decoderDesc struct {
-}
-
-func (dd decoderDesc) Make(pp, dk, dv string) (interface{}, error) {
-	return TypeFactory(fmt.Sprintf("%s(%s=%s)", pp, dk, dv))
-}
-
-func (dd decoderDesc) DiscriminatorFor(path string) string {
-	disc, ok := DiscriminatedOneOfSchemaMap[path]
-	if !ok {
-		return ""
-	}
-	return disc
-}
-
-// Bark defines model for Bark.
-type Bark struct {
-	Type   *string `json:"type,omitempty"`
-	Volume *int    `json:"volume,omitempty"`
-}
-
-// BasePet defines model for BasePet.
-type BasePet struct {
-	Age            *int           `json:"age,omitempty"`
-	Classes        *[]PetHeritage `json:"classes,omitempty"`
-	Heritage       *PetHeritage   `json:"heritage,omitempty"`
-	Name           *string        `json:"name,omitempty"`
-	NestedHeritage interface{}    `json:"nestedHeritage,omitempty"`
-	Species        interface{}    `json:"species,omitempty"`
-}
-
-// Cat defines model for Cat.
-type Cat struct {
-	Action interface{} `json:"action,omitempty"`
-	Mood   *string     `json:"mood,omitempty"`
-	Type   *string     `json:"type,omitempty"`
-}
-
-// Dog defines model for Dog.
-type Dog struct {
-	Action interface{} `json:"action,omitempty"`
-	Kind   *string     `json:"kind,omitempty"`
-	Type   *string     `json:"type,omitempty"`
-}
-
-// House defines model for House.
-type House struct {
-	Name  *string `json:"name,omitempty"`
-	Rooms *int    `json:"rooms,omitempty"`
-	Type  *string `json:"type,omitempty"`
-}
-
-// Meow defines model for Meow.
-type Meow struct {
-	Squeel *string `json:"squeel,omitempty"`
-	Type   *string `json:"type,omitempty"`
-}
-
-// Palace defines model for Palace.
-type Palace struct {
-	Halls  *int    `json:"Halls,omitempty"`
-	Name   *string `json:"name,omitempty"`
-	Towers *int    `json:"towers,omitempty"`
-	Type   *string `json:"type,omitempty"`
-}
-
-// PetHeritage defines model for PetHeritage.
-type PetHeritage struct {
-	Class interface{} `json:"class,omitempty"`
-	Name  *string     `json:"name,omitempty"`
-}
-
-// Purr defines model for Purr.
-type Purr struct {
-	Heritage *PetHeritage `json:"heritage,omitempty"`
-	Type     *string      `json:"type,omitempty"`
-}
-
-// Shack defines model for Shack.
-type Shack struct {
-	Material *string `json:"material,omitempty"`
-	Name     *string `json:"name,omitempty"`
-	Type     *string `json:"type,omitempty"`
-}
-
-func TypeFactory(kind string) (interface{}, error) {
-	fm := map[string]func() interface{}{
-		"BasePet.heritage.class(type=House)":  NewHouse,
-		"BasePet.heritage.class(type=Palace)": NewPalace,
-		"BasePet.heritage.class(type=Shack)":  NewShack,
-		"BasePet.nestedHeritage(type=House)":  NewHouse,
-		"BasePet.nestedHeritage(type=Palace)": NewPalace,
-		"BasePet.species(type=Cat)":           NewCat,
-		"BasePet.species(type=Dog)":           NewDog,
-		"Cat.action(type=Meow)":               NewMeow,
-		"Cat.action(type=Purr)":               NewPurr,
-		"Dog.action(type=Bark)":               NewBark,
-		"PetHeritage.class(type=House)":       NewHouse,
-		"PetHeritage.class(type=Palace)":      NewPalace,
-		"PetHeritage.class(type=Shack)":       NewShack,
-		"Purr.heritage.class(type=House)":     NewHouse,
-		"Purr.heritage.class(type=Palace)":    NewPalace,
-		"Purr.heritage.class(type=Shack)":     NewShack,
-	}
-	f, ok := fm[kind]
-	if !ok {
-		return nil, fmt.Errorf("cannot find type %s", kind)
-	}
-	return f(), nil
-}
-
-// Map <Schema, Discriminator>
-var DiscriminatedOneOfSchemaMap = map[string]string{
-
-	"BasePet.heritage.class": "type",
-	"BasePet.nestedHeritage": "type",
-	"BasePet.species":        "type",
-	"Cat.action":             "type",
-	"Dog.action":             "type",
-	"PetHeritage.class":      "type",
-	"Purr.heritage.class":    "type",
-}
-
-func NewBark() interface{} {
-	_d := "Type"
-	return &Bark{Type: &_d}
-}
-
-func (r Bark) Discriminator() string {
-	return "type"
-}
-
-func NewCat() interface{} {
-	_d := "Type"
-	return &Cat{Type: &_d}
-}
-
-func (r Cat) Discriminator() string {
-	return "type"
-}
-
-func NewDog() interface{} {
-	_d := "Type"
-	return &Dog{Type: &_d}
-}
-
-func (r Dog) Discriminator() string {
-	return "type"
-}
-
-func NewHouse() interface{} {
-	_d := "Type"
-	return &House{Type: &_d}
-}
-
-func (r House) Discriminator() string {
-	return "type"
-}
-
-func NewMeow() interface{} {
-	_d := "Type"
-	return &Meow{Type: &_d}
-}
-
-func (r Meow) Discriminator() string {
-	return "type"
-}
-
-func NewPalace() interface{} {
-	_d := "Type"
-	return &Palace{Type: &_d}
-}
-
-func (r Palace) Discriminator() string {
-	return "type"
-}
-
-func NewPurr() interface{} {
-	_d := "Type"
-	return &Purr{Type: &_d}
-}
-
-func (r Purr) Discriminator() string {
-	return "type"
-}
-
-func NewShack() interface{} {
-	_d := "Type"
-	return &Shack{Type: &_d}
-}
-
-func (r Shack) Discriminator() string {
-	return "type"
+	Owners []*PetOwner
 }
 
 func MyTestFactory(kind string) (interface{}, error) {
@@ -274,82 +81,6 @@ func MyTestFactory(kind string) (interface{}, error) {
 	}
 	return f(), nil
 }
-
-var oneOfTestPayload = `
-{
-    "pets" : [
-		{
-			"name": "Felix",
-			"age": 1,
-			"nestedHeritage": {
-			  "type": "House",
-			  "rooms": 8
-			},
-			"species": {
-				"type": "Cat",
-				"kind": "ALOOF",
-				"action": {
-					"type": "Purr",
-					"heritage": {
-						"name": "Buckingham",
-						"class": {
-							"type": "Palace",
-							"halls": 7,
-							"rooms": 20
-						}
-					}
-				}
-			},
-			"classes": [
-				{
-					"name": "Taj Mahal",
-					"class": {
-						"type": "Palace",
-						"halls": 27,
-						"rooms": 10
-					}
-				},
-				{
-					"name": "White",
-					"class": {
-						"type": "House",
-						"halls": 12,
-						"rooms": 100
-					}
-				}
-			],
-			"heritage": {
-				"name": "Little house in the prairie",
-				"class": {
-					"type": "House",
-					"rooms": 20
-				}
-			}
-		},
-		
-		{
-			"name": "Jerry",
-			"age": 1,
-			"subtype": {
-				"type": "Dog",
-				"kind": "SHEPHERD",
-				"action": {
-					"type": "Bark",
-					"volume": 7
-				}
-			},
-			"species": {
-				"type": "Dog",
-				"kind": "SHEPHERD",
-				"action": {
-					"type": "Bark",
-					"volume": 7
-				}
-			}
-		}
-    ]
-}
-`
 
 func TestDecodeNestedObject(t *testing.T) {
 
@@ -370,6 +101,18 @@ func TestDecodeNestedObject(t *testing.T) {
 		_, err := decode.Decode(m, "name", MyTestFactory)
 		So(err, ShouldNotBeNil)
 	})
+
+	// todo: this panics because the expectation is that sub is an object, not base type, but should defend
+	//Convey("unrully child object - assigned wrong type", t, func() {
+	//	mp := map[string]interface{}{
+	//		"name":  "foo",
+	//		"kind":  "record",
+	//		"slice": []string{"foo", "bar"},
+	//		"sub": "12",
+	//	}
+	//	_, err := decode.Decode(mp, "kind", MyTestFactory)
+	//	So(err, ShouldNotBeNil)
+	//})
 	Convey("unrully child object", t, func() {
 		mp := map[string]interface{}{
 			"name":  "foo",
@@ -611,10 +354,80 @@ func TestDecodeNestedObject(t *testing.T) {
 		_, err = decode.UnmarshalJSON(b[1:], "kind", MyTestFactory)
 		So(err, ShouldNotBeNil)
 	})
-	Convey("Test OneOf decoding", t, func() {
-		v, err := decode.UnmarshalJSONInto([]byte(oneOfTestPayload), &Envelope{}, &decoderDesc{})
+	Convey("Test OneOf decoding - pets1.json", t, func() {
+		// load spec from testdata identified by file
+		bytes, err := ioutil.ReadFile("testdata/pets1.json")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		v, err := decode.UnmarshalJSONInto(bytes, &Envelope{}, SchemaPathFactory)
 		So(err, ShouldBeNil)
 		_, err = json.MarshalIndent(v, "", "  ")
 		So(err, ShouldBeNil)
+	})
+	Convey("Test OneOf decoding - array of objects", t, func() {
+		b := `{ "name": "john", "owns": [{ "type": "Palace"}, {"type": "House"}]}`
+		_, err := decode.UnmarshalJSONInto([]byte(b), &PetOwner{}, SchemaPathFactory)
+		So(err, ShouldBeNil)
+	})
+	Convey("Test OneOf decoding - array of user crafted objects ", t, func() {
+		var y = struct{ LivesIn *[]struct{ Age *int } }{}
+		var x = struct{ LivesIn []*struct{ Age *int } }{}
+		var z = struct{ LivesIn []*struct{ Age int } }{}
+		m := map[string]interface{}{
+			"livesIn": []map[string]interface{}{
+				{"age": 7},
+			},
+		}
+
+		_, err := decode.DecodeInto(m, &y, SchemaPathFactory)
+		So(err, ShouldBeNil)
+		_, err = decode.DecodeInto(m, &x, SchemaPathFactory)
+		So(err, ShouldBeNil)
+		_, err = decode.DecodeInto(m, &z, SchemaPathFactory)
+		So(err, ShouldNotBeNil)
+	})
+	Convey("Test OneOf decoding - array of objects - bad oneOf", t, func() {
+
+		b := `{ "name": "john", "owns": [{ "class": "Palace"}, {"class":12}]}`
+		_, err := decode.UnmarshalJSONInto([]byte(b), &PetOwner{}, SchemaPathFactory)
+		So(err, ShouldNotBeNil)
+	})
+	Convey("Test OneOf decoding - array of objects - bad property type", t, func() {
+		b := `{ "name": "john", "owns": [{ "class": { "type": "House", "rooms": "string"}}]}`
+		_, err := decode.UnmarshalJSONInto([]byte(b), &PetOwner{}, SchemaPathFactory)
+		So(err, ShouldNotBeNil)
+	})
+	Convey("Test OneOf decoding - wrong oneOf discriminator", t, func() {
+		b := `{ "name": "john", "livesIn": { "class": "Palace"}}`
+		_, err := decode.UnmarshalJSONInto([]byte(b), &PetOwner{}, SchemaPathFactory)
+		So(err, ShouldNotBeNil)
+	})
+	Convey("Test OneOf decoding - bad json", t, func() {
+		b := `{ "name": `
+		_, err := decode.UnmarshalJSONInto([]byte(b), &PetOwner{}, SchemaPathFactory)
+		So(err, ShouldNotBeNil)
+	})
+	Convey("Test OneOf decoding - cannot decode into object that has non pointer fields", t, func() {
+		var x = struct {
+			Name    string
+			LivesIn []string
+		}{}
+		b := `{ "livesIn": { "class": "Palace"}}`
+		_, err := decode.UnmarshalJSONInto([]byte(b), &x, SchemaPathFactory)
+		So(err, ShouldNotBeNil)
+		var y = struct{ LivesIn *struct{ Age int } }{}
+		b = `{ "livesIn": { "age": 7}}`
+		_, err = decode.UnmarshalJSONInto([]byte(b), &y, SchemaPathFactory)
+		So(err, ShouldNotBeNil)
+	})
+	Convey("Test OneOf decoding - cannot decode into object is not struct pointer", t, func() {
+		b := `{ "name": "john"}`
+		i := 1
+		_, err := decode.UnmarshalJSONInto([]byte(b), i, SchemaPathFactory)
+		So(err, ShouldNotBeNil)
+		_, err = decode.UnmarshalJSONInto([]byte(b), &i, SchemaPathFactory)
+		So(err, ShouldNotBeNil)
 	})
 }
